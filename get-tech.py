703d5a93ee2afe9be0143378d2afabc9313db4c5
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2021-12-09 17:50:09 -0300

    Show skip message when not tech was found

--- get-tech.py
+++ get-tech.py
@@ -7,8 +7,14 @@ with open("techs.json") as file:
     for url, techs in data['technologies'].items():
         print(f">>> Inserting technologies for {url}")
 
+        if not techs:
+            print("No tech found to insert, skipping...")
+            continue
+
         for tech in techs:
             print(f"Inserting {tech}...")
 
             run = f'python3 insert-data.py -u "{url}" -t "{tech}"'
             os.system(run)
+
+        print()
